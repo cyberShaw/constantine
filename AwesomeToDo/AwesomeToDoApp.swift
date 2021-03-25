bc69a7bc8909da4fd9738078f93ab0a6dadfdf33
Saiharsha B <saiharsha.b@flipkart.com>
2021-03-25 20:01:38 +0530

    Initial Commit

---
+++ AwesomeToDo/AwesomeToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeToDoApp.swift
+//  AwesomeToDo
+//
+//  Created by Apollo on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
